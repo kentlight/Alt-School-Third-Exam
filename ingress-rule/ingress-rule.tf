facbe961fc1dd6102aebfff96553ae30eeb592b2
kentlight <kentlightosa.com>
2023-03-19 15:59:43 +0000

    add

---
+++ ingress-rule/ingress-rule.tf
@@ -0,0 +1,61 @@
+resource "kubernetes_ingress_v1" "voting-ingress" {
+  metadata {
+    name      = "vote-front"
+    namespace = "voting-app"
+    labels = {
+      name = "vote-front"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "voting.osayamenimar.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "azure-vote-front"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "micro-ingress" {
+  metadata {
+    name      = "sock-shop"
+    namespace = "sock-shop"
+    labels = {
+      name = "front-end"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "socks-shop.osayamenimar.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "front-end"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
